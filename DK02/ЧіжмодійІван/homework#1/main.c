7d2bdaf3944e40a18ca457d135229c44db1b8634
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-02-10 21:59:10 +0200

    Create main.c

---
+++ DK02/ЧіжмодійІван/homework#1/main.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include"function.h"
+
+
+int main(void)
+{
+	printf("Hello world\n");
+	printf("Enter a number if you want to run the program, 0 if not: "); 
+	
+
+	printf("%d", build_matrix(4, 4));
+	
+
+}
