e05ecefc52c6d994f446f3d0b8a4177986521cf7
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-02-11 23:04:25 +0200

    Create main.c

---
+++ DK02/ЧіжмодійІван/lab#1/main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include"math.h"
+
+int main(void)
+{
+	
+	printf("Hello world\n");
+	
+	
+	printf("The result of the calculation: %f\n",result(3,5,5));
+
+	
+
+	
+}
