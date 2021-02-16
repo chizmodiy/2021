d21482e934182c1c5f1b99bb66a8e7cfd959fc56
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-02-16 17:27:34 +0200

    Update math.h

--- DK02/ЧіжмодійІван/lab#1/math.h
+++ DK02/ЧіжмодійІван/lab#1/math.h
@@ -1,8 +1,16 @@
+
 #pragma once
+#include <stdio.h>
+#define MATH_H
+#ifdef MATH_H
 
 
-double q(int A, int B, int C);
-long factorial(int number);
-long sum_factor(int start, int finish);
+
+float factorial(int number);
+float sum_factor(int start, int finish);
 double result(int A, int B, int C);
-int my_sqr(int number);
+float my_sqr(int number);
+float dil(float a, float b);
+#endif
+
+
