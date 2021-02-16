a7549ba117e976452175669f83efdce6a1c82bd4
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-02-16 17:27:04 +0200

    Update math.c

--- DK02/ЧіжмодійІван/lab#1/math.c
+++ DK02/ЧіжмодійІван/lab#1/math.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+#include"math.h"
 
 
 
 
-long factorial(int number) {
+float factorial(int number) {
 	long result = 1;
 
 	if (number < 0) {
 		printf("Error...(<0)\n\t");
-		
+
 	}
 
 	for (int i = 1; i <= number; i++) { // create factorial
@@ -19,13 +20,13 @@ long factorial(int number) {
 }
 
 
-long sum_factor(int start, int finish) 
+float sum_factor(int start, int finish)
 {
 	long result = 0;
 	if (start < 0) start = -start;
 	if (finish < 0) finish = -finish;
 
-	for (int i = start; i < finish; i++) 
+	for (int i = start; i < finish; i++)
 	{  // for factorial make sum of numbers
 		result += factorial(i + finish);
 	}
@@ -35,25 +36,38 @@ long sum_factor(int start, int finish)
 
 
 
-
-double my_sqr(int number) {
+float  my_sqr(int number) {
 	int temp = 0, x = 1;
 	if (number < 0)
 	{
 		printf("Error...(<0)\n\t");
 	}
-		
+
 
 	while (temp != x) {
 		temp = number / x;
 		if (temp == x) return x;
 		x++;
 	}
-	
+
 }
 
-float result(int A, int B, int C)
+double result(int A, int B, int C)
 {
-	return (float)A * (float)((my_sqr(B + C) / B)) + (float)sum_factor(0, A);  // function for print result of functions
+	return (double)A * (double) (  dil( my_sqr (B + C)  , B ) + (double)sum_factor(0, A));  // function for print result of functions
+}
+
+float dil(float a, float b)
+{
+	if (b == 0)
+	{
+		printf("\tError! Values greater than 0\n");
+		
+
+	}
+	else
+	{
+		return (float)(a / b);
+	}
 }
 
