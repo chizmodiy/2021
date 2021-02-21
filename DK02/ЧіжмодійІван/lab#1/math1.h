b3d0f3ac1efc2853d8b077c6534bba5969a92560
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-02-21 21:38:18 +0200

    Update math1.h

--- DK02/ЧіжмодійІван/lab#1/math1.h
+++ DK02/ЧіжмодійІван/lab#1/math1.h
@@ -1,16 +1,11 @@
-
 #pragma once
 #include <stdio.h>
-#include <math.h>
+#include<math.h>
 #define MATH1_H
 #ifdef MATH1_H
-
-
 
 float factorial(int number);
 float sum_factor(int start, int finish);
 double result(int A, int B, int C);
 float dil(float a, float b);
 #endif
-
-
