0e84a42422ae5299740aee1d2ce9fb82e7900bbe
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-03-01 21:27:09 +0200

    Create ellipse.h

---
+++ DK02/ЧіжмодійІван/hw2/ellipse/ellipse.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef HOMEWORKN2_ELLIPSE_H
+#define HOMEWORKN2_ELLIPSE_H
+
+#endif //HOMEWORKN2_ELLIPSE_H
+#include "point.h"
+typedef struct tag_ellipse {
+
+    Point* f1;
+    Point* f2;
+    int a;
+    int b;
+    int focusDistance;
+
+} Ellipse;
+
+Ellipse* createByFocusesEllipse(Point* f1, Point* f2, int a, int b);
+Ellipse* createByParametersEllipse(int a, int b);
+Ellipse* createEllipseByCoords(int x1, int x2, int a, int b);
+Ellipse* copyEllipse(Ellipse* l);
+int isPointBelongsToEllipse(Point p, Ellipse e);
+int isEqualEllipse(Ellipse* ellipse1, Ellipse* ellipse2);
+double focusLenght(Ellipse* ellipse);
+void deleteEllipse(Ellipse* ellipse);
+void printEllipse(Ellipse* ellipse);
