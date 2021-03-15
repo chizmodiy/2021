e1d4b3efc692eaf4daffa6318bb47088670b690c
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-03-15 18:19:33 +0200

    Create main.c

---
+++ DK02/ЧіжмодійІван/laba#2/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"list.h"
+
+int main()
+{
+
+	int* new_list = make_list_item(4);
+	
+	add_value_to_list(new_list, 61);
+	add_value_to_list(new_list, 7);
+	add_value_to_list(new_list, 27);
+	print_list(new_list);
+	print_list_reverse(new_list);
+	print_list(new_list);
+	print_list_reverse(new_list);
+	delete_list(new_list);
+
+}
