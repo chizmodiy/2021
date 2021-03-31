5d971461facaffdc121e8ad8326a532a26f45728
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2021-04-01 00:02:03 +0300

    Update main.c

--- DK02/ЧіжмодійІван/laba#2/main.c
+++ DK02/ЧіжмодійІван/laba#2/main.c
@@ -3,15 +3,17 @@
 
 int main()
 {
-	make_list();
-	int* new_list = make_list_item(4);
-	
+
+	int* new_list = make_list();
+
 	add_value_to_list(new_list, 61);
 	add_value_to_list(new_list, 7);
 	add_value_to_list(new_list, 27);
 	print_list(new_list);
+	printf("Reverve list :");
 	print_list_reverse(new_list);
 	print_list(new_list);
+	printf("Reverve list :");
 	print_list_reverse(new_list);
 	delete_list(new_list);
 
